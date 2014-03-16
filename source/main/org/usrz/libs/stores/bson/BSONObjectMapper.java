796b623c8b8801e404144c594f244e10ab170c1d
Pier Fumagalli <pier@fumagalli.org>
2014-03-16 19:17:31 +0900

    Naming strategy and views support.

--- source/main/org/usrz/libs/stores/bson/BSONObjectMapper.java
+++ source/main/org/usrz/libs/stores/bson/BSONObjectMapper.java
@@ -15,6 +15,8 @@
  * ========================================================================== */
 package org.usrz.libs.stores.bson;
 
+import static com.fasterxml.jackson.databind.PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES;
+
 import java.io.IOException;
 import java.util.Map;
 
@@ -25,6 +27,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.module.SimpleDeserializers;
 import com.fasterxml.jackson.databind.module.SimpleSerializers;
 import com.mongodb.BasicDBObject;
@@ -40,6 +44,9 @@ public class BSONObjectMapper extends ObjectMapper {
     public BSONObjectMapper(ObjectMapper mapper) {
         /* Copy base configurations (or default if null) */
         super(mapper == null ? new ObjectMapper() : mapper);
+
+        /* Always use underscores in names */
+        setPropertyNamingStrategy(CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
 
         /*
          * We can't use a module here, as they seem to be shared across all
@@ -63,20 +70,48 @@ public class BSONObjectMapper extends ObjectMapper {
                         .withAdditionalDeserializers(deserializers));
     }
 
+    /* ====================================================================== */
+
     public <T> T readValue(BSONObject object, Class<T> type)
     throws JsonProcessingException, IOException {
-        return reader(type).readValue(new BSONParser(this, object));
+        return readBSON(reader(type), object, type);
     }
+
+    public <T> T readValueWithView(BSONObject object, Class<T> type, Class<?> view)
+    throws JsonProcessingException, IOException {
+        if (view == null) throw new NullPointerException("Null view");
+        return readBSON(readerWithView(view), object, type);
+    }
+
+    private <T> T readBSON(ObjectReader reader, BSONObject object, Class<T> type)
+    throws JsonProcessingException, IOException {
+        return reader.readValue(new BSONParser(this, object), type);
+    }
+
+    /* ====================================================================== */
 
     public BasicDBObject writeValueAsBson(Object object)
     throws JsonGenerationException, JsonMappingException, IOException {
+        return writeBson(writer(), object);
+    }
+
+    public BasicDBObject writeValueAsBsonWithView(Object object, Class<?> view)
+    throws JsonGenerationException, JsonMappingException, IOException {
+        if (view == null) throw new NullPointerException("Null view");
+        return writeBson(writerWithView(view), object);
+    }
+
+    private BasicDBObject writeBson(ObjectWriter writer, Object object)
+    throws JsonGenerationException, JsonMappingException, IOException {
         final BSONGenerator generator = new BSONGenerator(this);
-        writer().writeValue(generator, object);
+        writer.writeValue(generator, object);
         final BSONObject bson = generator.getOutputTarget();
         if (bson instanceof BasicDBObject) return (BasicDBObject) bson;
         final Map<?, ?> map = bson instanceof Map ? (Map<?, ?>) bson : bson.toMap();
         return new BasicDBObject(map);
     }
+
+    /* ====================================================================== */
 
     @Override
     public BSONObjectMapper copy() {
